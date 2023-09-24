4e08e015a497f51a526bd69ad927d087535695b2
decl1 <77211193+decl1@users.noreply.github.com>
2023-09-24 19:52:31 +0300

    stl ex

--- gtest/STL/Header.h
+++ gtest/STL/Header.h
@@ -1,5 +1,6 @@
 #pragma once 
 #include <vector>
+#include <algorithm>
 #include <stdlib.h> 
 
 void fillvectortwentyzero(std::vector<int>& vec) {
@@ -11,7 +12,47 @@ void fillvectortwentyzero(std::vector<int>& vec) {
 };
 
 int randvecset(std::vector<int>& vec) {
-	vec.push_back(rand() % 200 + 1 - 100);
-	auto iter = vec.end();
+	int rand_set = (rand() % 200 + 1 - 100);
+	vec.push_back(rand_set);
+	auto iter = vec.end() - 1;
 	return (*iter);
+}
+
+std::pair<int,int> outpair(const std::vector<int>& vec) {
+	int locmin = (*vec.begin());
+	int locmax = locmin;
+	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+		if ((*iter) > locmax)
+			locmax = (*iter);
+		if ((*iter) < locmin)
+			locmin = (*iter);
+	}
+	std::pair<int, int> result(locmin, locmax);
+	return result;
+}
+
+void addtoend(std::vector<int>& vec, int array[], int size) {
+	for (int i = 0; i < size; i++) {
+		vec.push_back(array[i]);
+	}
+	sort(vec.begin(), vec.end());
+}
+
+void cmpvectozero(std::vector<int>& vec, int cmp) {
+	std::replace_if(vec.begin(), vec.end(), [cmp](int& i) {
+		return i < cmp;
+		}, 0);
+}
+
+std::vector<int> createvectorbiggerthancmp(std::vector<int> vec, int cmp) {
+	std::vector<int> result;
+	for (auto i : vec) {
+		if (i > cmp)
+			result.push_back(i);
+	}
+	return result;
+}
+
+void erasevector(std::vector<int>& vec) {
+	vec.clear();
 }
--- gtest/tests/test_STL.cpp
+++ gtest/tests/test_STL.cpp
@@ -37,3 +37,74 @@ TEST(test_setrandvec, check_rand) {
 	}
 	EXPECT_EQ(test_complete, 1);
 }
+
+///
+
+TEST(test_minmax_out, checkmin) {
+	bool all_correct = 0;
+	std::vector<int> vec = {5 , 2, -3, 10, 2};
+
+	EXPECT_EQ(outpair(vec).first, -3);
+}
+
+TEST(test_minmax_out, checkmax) {
+	bool all_correct = 0;
+	std::vector<int> vec = { 5 , 2, -3, 10, 2 };
+
+	EXPECT_EQ(outpair(vec).second, 10);
+}
+
+///
+
+TEST(test_arrayaddtovec, check_size) {
+	std::vector<int> vec = { 6, 2 ,1 ,5 };
+	int array[4] = { 5,2,6,9 };
+
+	addtoend(vec, array,4);
+
+	EXPECT_EQ(vec.size(), 8);
+}
+
+TEST(test_arrayaddtovec, check_min) {
+	std::vector<int> vec = { 6, 2 ,1 ,5 };
+	int array[4] = { 5,2,6,9 };
+
+	addtoend(vec, array,4);
+
+	EXPECT_EQ(*vec.begin(), 1);
+}
+TEST(test_arrayaddtovec, check_max) {
+	std::vector<int> vec = { 6, 2 ,1 ,5 };
+	int array[4] = { 5,2,6,9 };
+
+	addtoend(vec, array, 4);
+
+	EXPECT_EQ(*(vec.end()-1), 9);
+}
+
+///
+
+TEST(test_cmptozero, check_vec) {
+	std::vector<int> vec = { 6,2,1,5 };
+
+	cmpvectozero(vec, 3);
+	EXPECT_EQ(vec[0] + vec[3], 11);
+
+}
+
+///
+
+TEST(createvectorbiggerthancmp__, check_array) {
+	std::vector<int> vec = { 6, 2 ,1 ,5 };
+	std::vector<int> veccmp = createvectorbiggerthancmp(vec, 3);
+
+	EXPECT_EQ(veccmp[0] == 6 && veccmp[1] == 5, 1);
+}
+
+///
+
+TEST(erasevector_, check_erased) {
+	std::vector<int> vec = { 1,2,3,4,5,6,7 };
+	erasevector(vec);
+	EXPECT_EQ(vec.size(), 0);
+}
